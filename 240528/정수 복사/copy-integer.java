4b751fe419ed7c9d84cce6ca9c3eda095295ea14
Inu <153512886+innoo47@users.noreply.github.com>
2024-05-28 21:23:40 +0900

    Title: 정수 복사, Time: 78ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240528/정수 복사/copy-integer.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 3;
+        int b = 4;
+
+        b = a;
+
+        System.out.printf("%s %s\r\n", a, b);
+        System.out.printf("%s", (a*b));
+    }
+}
