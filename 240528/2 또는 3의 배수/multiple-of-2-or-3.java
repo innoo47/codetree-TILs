551bc03df101cc86186dad0d3c450b9d65fdec66
Inu <153512886+innoo47@users.noreply.github.com>
2024-05-28 20:15:30 +0900

    Title: 2 또는 3의 배수, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240528/2 또는 3의 배수/multiple-of-2-or-3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1;i<= n;++i) {
+            if(i % 2 == 0 || i %3 == 0) {
+                System.out.print("1 ");
+            } else {
+                System.out.print("0 ");
+            }
+        }
+
+    }
+}
