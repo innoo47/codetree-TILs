e91710661c25f17925a61d1c9bb40115215e7e08
Inu <153512886+innoo47@users.noreply.github.com>
2024-05-28 21:39:43 +0900

    Title: 데이터 교환, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240528/데이터 교환/exchange-data.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 5;
+        int b = 6;
+        int c = 7;
+
+        int x = b;
+        int y = c;
+
+        b = a;
+        c = x;
+        a = y;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+
+
+        
+    }
+}
