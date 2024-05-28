eef40a52aea4a98b97f9b9c9e9c27c4f9978e02f
Inu <153512886+innoo47@users.noreply.github.com>
2024-05-28 21:43:01 +0900

    Title: 합을 복사하기, Time: 99ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240528/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+
+        int result = a + b + c;
+
+        a = result;
+        b = result;
+        c = result;
+
+        System.out.printf("%s %s %s", a, b, c);
+        
+    }
+}
